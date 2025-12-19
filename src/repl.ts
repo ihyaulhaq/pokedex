f30feac1676c48fc47bcea44cda3af88ab7ec80d
ihyaulhaq <138434786+ihyaulhaq@users.noreply.github.com>
2025-12-19 11:35:26 +0700

    Feat: read input from command line

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,24 @@
+import { createInterface } from "node:readline";
+import { stdin, stdout } from "node:process";
+
 export function cleanInput(input: string): string[] {
   return input.toLowerCase().trim().split(/\s+/);
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'pokedex > '
+  });
+  rl.prompt()
+  rl.on("line", (line)=>{
+    const command = cleanInput(line)
+    if(command.length === 0){
+      rl.prompt()
+      return
+    }
+    console.log(`Your command was: ${command[0]}`)
+    rl.prompt()
+  })
 }
